e5555ad034cb14d4f3eadbe67ab7cac461e5fd93
AdlerCastro <adlercastro01@gmail.com>
2024-08-13 18:36:01 -0300

    :sparkles: feat: Strings

---
+++ src/components/molecules/Frets/index.tsx
@@ -0,0 +1,12 @@
+export default function Frets() {
+  return (
+    <div className="w-24 grid grid-rows-6 gap-y-2 text-center">
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+      <input type="checkbox" />
+    </div>
+  );
+}
