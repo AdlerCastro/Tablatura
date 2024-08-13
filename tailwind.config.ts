dda10bee069e58dc433dce91a0c92e892138996b
AdlerCastro <adlercastro01@gmail.com>
2024-08-13 18:24:26 -0300

    :lipstick: chore: Variáveis CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        poppins: ['var(--font-poppins)']
+      },
+      gridTemplateRows: {
+        app: 'min-content max-content',
+      },
+      gridTemplateColumns: {
+        Neck: '100px 6px 100px 6px 100px 6px',
       },
     },
   },
