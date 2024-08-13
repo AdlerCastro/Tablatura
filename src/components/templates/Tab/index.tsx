0ea2e9c368e8c28eeff81e29c8c3da5c42a7efe8
AdlerCastro <adlercastro01@gmail.com>
2024-08-13 18:36:50 -0300

    :sparkles: feat: Tablature

---
+++ src/components/templates/Tab/index.tsx
@@ -0,0 +1,12 @@
+import Neck from "@/components/organisms/Neck";
+
+export default function Tab() {
+  return (
+    <div className="flex flex-col w-full h-full items-center gap-5">
+      <h2>Tablatura complete</h2>
+      <div>
+        <Neck />
+      </div>
+    </div>
+  );
+}
