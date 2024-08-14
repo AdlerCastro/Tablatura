5269639a33841771e434ee04a110678d7b52c349
AdlerCastro <adlercastro01@gmail.com>
2024-08-14 18:52:44 -0300

    :sparkles: style: Props do input

--- src/components/molecules/Frets/index.tsx
+++ src/components/molecules/Frets/index.tsx
@@ -1,12 +1,15 @@
-export default function Frets() {
+interface FretProps {
+  id: string;
+}
+
+export default function Frets({ id }: FretProps) {
   return (
-    <div className="w-24 grid grid-rows-6 gap-y-2 text-center">
-      <input type="checkbox" />
-      <input type="checkbox" />
-      <input type="checkbox" />
-      <input type="checkbox" />
-      <input type="checkbox" />
-      <input type="checkbox" />
+    <div className="grid grid-rows-6 grid-cols-1 w-full h-full text-center gap-y-5">
+      <input type="checkbox" name={id} id={id} />
+      <label
+        htmlFor={id}
+        className="border border-solid bg-zinc-500 rounded-full h-3 group-checked:bg-zinc-50"
+      ></label>
     </div>
   );
 }
