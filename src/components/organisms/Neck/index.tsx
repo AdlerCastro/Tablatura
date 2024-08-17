5adbb45d804ece3f0e8d01217b493b768a35c370
AdlerCastro <adlercastro01@gmail.com>
2024-08-17 19:33:42 -0300

    :construction: Tablature body

--- src/components/organisms/Neck/index.tsx
+++ src/components/organisms/Neck/index.tsx
@@ -1,23 +1,9 @@
-import Fret from "@/components/atoms/Fret";
-import Frets from "@/components/molecules/Frets";
+import React from "react";
 
 export default function Neck() {
   return (
-    <div className="group grid grid-rows-6 grid-cols-Frets justify-center bg-zinc-800 rounded-md py-5 px-2 items-center gap-2">
-      <Frets id={"F1FirstFret"} />
-      <Fret />
-
-      <Frets id={"F2FirstFret"} />
-      <Fret />
-
-      <Frets id={"F3FirstFret"} />
-      <Fret />
-
-      <Frets id={"F3FirstFret"} />
-      <Fret />
-
-      <Frets id={"F3FirstFret"} />
-      <Fret />
+    <div>
+      <h2>Neck</h2>
     </div>
   );
 }
