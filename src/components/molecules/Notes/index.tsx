389a9d313d3493222f3d44d6f445b6fb3614d1b3
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 03:14:43 -0300

    :sparkles: feat: Add function to styling on Pattern component

--- src/components/molecules/Notes/index.tsx
+++ src/components/molecules/Notes/index.tsx
@@ -1,16 +1,35 @@
+"use client";
+
 import Button from "@/components/atoms/Button";
 
-export default function Notes() {
+interface NotesProps {
+  onClickChange: () => void;
+}
+
+export default function Notes({ onClickChange }: NotesProps) {
   return (
     <div className="flex flex-col gap-3 w-full">
-      <Button className="w-full hover:scale-110">C</Button>
-      <Button className="w-full hover:scale-110">D</Button>
-      <Button className="w-full hover:scale-110">E</Button>
-      <Button className="w-full hover:scale-110">F</Button>
-      <Button className="w-full hover:scale-110">G</Button>
-      <Button className="w-full hover:scale-110">A</Button>
-      <Button className="w-full hover:scale-110">B</Button>
-      <Button className="w-full hover:scale-110">C</Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        C
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        D
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        E
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        F
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        G
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        A
+      </Button>
+      <Button className="w-full hover:scale-105" onClick={onClickChange}>
+        B
+      </Button>
     </div>
   );
 }
