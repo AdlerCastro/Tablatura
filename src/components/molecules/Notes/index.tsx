4f4c828ae6b92d094ed3e11adff3c8892bd20da5
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 17:03:31 -0300

    :sparkles: feat: GET Notes to db

--- src/components/molecules/Notes/index.tsx
+++ src/components/molecules/Notes/index.tsx
@@ -1,35 +1,32 @@
 "use client";
 
+import { getNotes } from "@/app/actions/getNotes";
+import { useQuery } from "@tanstack/react-query";
 import Button from "@/components/atoms/Button";
+import { useState } from "react";
+import { Note } from "@/data/types/notes";
 
 interface NotesProps {
-  onClickChange: () => void;
+  onSendNote: (data: Note) => void;
 }
 
-export default function Notes({ onClickChange }: NotesProps) {
+export default function Notes({ onSendNote }: NotesProps) {
+  const { data, isLoading } = useQuery({
+    queryFn: async () => await getNotes(),
+    queryKey: ["Notes"],
+  });
+
   return (
     <div className="flex flex-col gap-3 w-full">
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        C
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        D
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        E
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        F
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        G
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        A
-      </Button>
-      <Button className="w-full hover:scale-105" onClick={onClickChange}>
-        B
-      </Button>
+      {data?.map((note) => (
+        <Button
+          key={note.id}
+          onClick={() => onSendNote(note)}
+          className="w-full hover:scale-105"
+        >
+          {note.title}
+        </Button>
+      ))}
     </div>
   );
 }
