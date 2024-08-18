4d3fe161462ebb4ffa1526b2f6ad99c34ac6b91f
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 18:46:24 -0300

    :recycle: refactor: Adjust visibility on browser for developer

--- src/app/api/Notes/route.ts
+++ src/app/api/Notes/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server"
 import data from "./data.json"
 export async function GET() {
     try {
-        return new NextResponse(JSON.stringify(data.notes), {status: 200})
+        return NextResponse.json(data.notes, {status: 200})
         
     } catch (error) {
-        return new NextResponse(`Erro ao resgatar as notas: ${error}`, {status: 501})
+        return NextResponse.json(`Erro ao resgatar as notas: ${error}`, {status: 501})
     }
 }
