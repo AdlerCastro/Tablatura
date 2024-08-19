702fb33175460f897654bc19fcaa8b89c490441f
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 21:06:14 -0300

    :ok_hand: style: Adjust andress route

--- src/app/actions/getNotes.tsx
+++ src/app/actions/getNotes.tsx
@@ -4,7 +4,7 @@ import { api } from "@/data/api";
 import { Note } from "@/data/types/notes";
 
 export async function getNotes(): Promise<Note[]> {
-  const response = await api("/Notes", {
+  const response = await api("/Notes/getNotes", {
     next: {
       revalidate: 60 * 2,
     },
