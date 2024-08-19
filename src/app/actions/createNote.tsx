85780c33656d41b5ca47a8d5ddb435de486a8fba
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 21:05:43 -0300

    :sparkles: feat(action): Create a create note function

---
+++ src/app/actions/createNote.tsx
@@ -0,0 +1,22 @@
+"use server";
+
+import { api } from "@/data/api";
+import { NoteFormData } from "@/data/types/NoteFormData";
+
+// Função para criar a nota
+export async function createNote(newNote: NoteFormData) {
+  const response = await api("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newNote), // Certifique-se de enviar `newNote`
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error || "Erro ao adicionar a nota");
+  }
+
+  return response.json();
+}
