4a3d0dee814bfb170e677e4c2d4b8f9da4eb96d2
AdlerCastro <adlercastro01@gmail.com>
2024-08-18 22:25:43 -0300

    :ok_hand: feat: Upload instantly

--- src/app/actions/getNotes.tsx
+++ src/app/actions/getNotes.tsx
@@ -5,9 +5,9 @@ import { Note } from "@/data/types/notes";
 
 export async function getNotes(): Promise<Note[]> {
   const response = await api("/Notes/getNotes", {
-    next: {
-      revalidate: 60 * 2,
-    },
+    // next: {
+    //   revalidate: 60 * 2,
+    // },
   });
 
   const notes = await response.json();
