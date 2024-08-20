967c438a483138c195a9554c48bf8327879b4ac2
AdlerCastro <adlercastro01@gmail.com>
2024-08-20 19:21:54 -0300

    :sparkles: feat: Add componente ShowNotesOnTab

--- src/components/organisms/Tab/index.tsx
+++ src/components/organisms/Tab/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import Neck from "../Neck";
+import ShowNotesOnTab from "../ShowNotesOnTab";
 
 export default function Tab() {
   return (
-    <div className="flex flex-col gap-2 p-4 w-full bg-zinc-800 rounded-lg border border-solid border-b-0 border-zinc-700 items-center">
+    <div className="flex flex-col gap-20 p-4 w-full bg-zinc-800 rounded-lg border border-solid border-b-0 border-zinc-700 items-center">
       <h2>Tablatura</h2>
-      <Neck />
+      <ShowNotesOnTab />
     </div>
   );
 }
